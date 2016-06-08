ecd84a06e8ec76b89684a07eb0bb377abf3843ee
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-06-07 18:12:29 -0600

    Setup oauth with google

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
 
+  get '/auth/google_oauth2', as: :google_login
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
 end
