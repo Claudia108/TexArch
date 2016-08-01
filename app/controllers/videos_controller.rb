8763fafd65c2d9cb52e097e68f1d0c81a4b26dc1
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-07-31 18:50:59 -0600

    Add after action to Videos Controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,6 @@
 class VideosController < ApplicationController
+  after_action :allow_youtube_iframe
+
   def index
     @videos = Video.all
   end
@@ -6,5 +8,12 @@ class VideosController < ApplicationController
   def show
     @video = Video.find(params[:id])
     @link = Video.youtube_embed(@video.link)
+  end
+
+
+private
+
+  def allow_youtube_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM https://www.youtube.com'
   end
 end
