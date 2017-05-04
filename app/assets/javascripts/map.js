c552fd9b8ff4fb8072fb92aa4cbb0bf46e97c71c
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2017-05-04 17:16:00 -0600

    Fix map error

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -22,41 +22,45 @@ var resetMap = function(map) {
 };
 
 var initMap = function(response) {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: zoom,
-    center: centerPoint,
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  });
+  function initialize() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: zoom,
+      center: centerPoint,
+      mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+    google.maps.event.addDomListener(window, "load", initialize);
 
-  var setMarkers = function(response) {
-    if (typeof response !== "undefined") {
-      response.forEach(function(site) {
-        var coordinates = { lng: parseFloat(site.longitude), lat: parseFloat(site.latitude) };
-        var marker = new google.maps.Marker({
-          position: coordinates,
-          map: map,
-          title: site.name,
-        });
 
-        var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">' + site.name + '</h1>'+
-            '<div id="bodyContent">'+
-            '<p>' + site.description + '</p>' +
-            '<p>Information: <a href="http://www.gaultschool.org/Home.aspx", target="_blank">' +
-            'http://www.gaultschool.org/Home.aspx' +
-            '</a></p>'+
-            // '<img src="http://www.texasbeyondhistory.net/gault/images/2001tamu-sm2.jpg", width="250px"' +
-            '</div>' + '</div>';
 
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
+    var setMarkers = function(response) {
+      if (typeof response !== "undefined") {
+        response.forEach(function(site) {
+          var coordinates = { lng: parseFloat(site.longitude), lat: parseFloat(site.latitude) };
+          var marker = new google.maps.Marker({
+            position: coordinates,
+            map: map,
+            title: site.name,
+          });
 
-        marker.addListener('mouseover', function () {
-          infowindow.open(map, marker);
-        });
+          var contentString = '<div id="content">'+
+              '<div id="siteNotice">'+
+              '</div>'+
+              '<h1 id="firstHeading" class="firstHeading">' + site.name + '</h1>'+
+              '<div id="bodyContent">'+
+              '<p>' + site.description + '</p>' +
+              '<p>Information: <a href="http://www.gaultschool.org/Home.aspx", target="_blank">' +
+              'http://www.gaultschool.org/Home.aspx' +
+              '</a></p>'+
+              // '<img src="http://www.texasbeyondhistory.net/gault/images/2001tamu-sm2.jpg", width="250px"' +
+              '</div>' + '</div>';
+
+          var infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
+
+          marker.addListener('mouseover', function () {
+            infowindow.open(map, marker);
+          });
 
           marker.addListener('mouseout', function () {
             if(map.zoom === 4) {
@@ -65,17 +69,19 @@ var initMap = function(response) {
           });
 
 
-        marker.addListener('click', function() {
-          map.setZoom(11);
-          map.setCenter(marker.getPosition());
-          infowindow.open(map, marker);
-        });
+          marker.addListener('click', function() {
+            map.setZoom(11);
+            map.setCenter(marker.getPosition());
+            infowindow.open(map, marker);
+          });
 
-        google.maps.event.addListener(infowindow,'closeclick',function(){
-          resetMap(map);
+          google.maps.event.addListener(infowindow,'closeclick',function(){
+            resetMap(map);
+          });
         });
-      });
-    }
-  };
-  setMarkers(response);
+      }
+    };
+    setMarkers(response);
+  }
+  google.maps.event.addDomListener(window, "load", initialize);
 };
