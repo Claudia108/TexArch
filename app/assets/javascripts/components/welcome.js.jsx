d1ad689fe23fba86f2d1f8e116c5eb62a0ae5be5
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2017-05-05 17:23:38 -0600

    Simplify home index view with react

--- app/assets/javascripts/components/welcome.js.jsx
+++ app/assets/javascripts/components/welcome.js.jsx
@@ -1,14 +1,24 @@
 class Welcome extends React.Component {
   render () {
     return (
-      <section className="left-side col-md-8 ">
-        <h4>TexArch is an online tool hosting data supportive to
-            archaeological research, with an emphasis
-            on the Texas Archaic period.
-            <a href="/calf_creek_horizon">  Learn more...</a>
-        </h4>
-        <img src="/images/landscape.jpg" className="welcome-image img-rounded" alt="Texas Landscape" />      
-      </section>
+      <main>
+        <h2>Welcome to TexArch </h2>
+        <div class="content-box center">
+          <div class="welcome">
+            <div class="row">
+              <section className="left-side col-md-8 ">
+                <h4>TexArch is an online tool hosting data supportive to
+                    archaeological research, with an emphasis
+                    on the Texas Archaic period.
+                    <a href="/calf_creek_horizon">  Learn more...</a>
+                </h4>
+                <img src="/images/landscape.jpg" className="welcome-image img-rounded" alt="Texas Landscape" />
+              </section>
+              <Author />
+            </div>
+          </div>
+        </div>
+      </main>
     );
   }
 };
