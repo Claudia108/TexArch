4e0a10744f58c49e98fa3665da80975a392e58fc
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2017-05-05 11:04:54 -0600

    Refactor map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,82 +1,66 @@
-$(document).ready(function() {
-    renderMap();
-});
-
 function renderMap() {
-  $.ajax({
-    method: "GET",
-    url: "/api/v1/sites.json",
-    dataType: "JSON",
-    success: function(response) {
-      initMap(response);
-    }
-  });
+  $.getJSON("/api/v1/sites.json").then(function(response) {
+    $('#map').each(function() {
+      var map = new google.maps.Map(this, {
+        zoom: zoom,
+        center: centerPoint,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+      });
+
+      response.forEach(setupMarkerForSite.bind(this, map));
+    })
+  })
 }
 
-var centerPoint = { lat: 39.50, lng: -98.35 };
-var zoom = 4;
+const centerPoint = { lat: 39.50, lng: -98.35 };
+const zoom = 4;
 
-var resetMap = function(map) {
+function resetMap(map) {
   map.setZoom(zoom);
   map.setCenter(centerPoint);
 };
 
-var initMap = function(response) {
-  if($("#map").length != 0) {
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: zoom,
-      center: centerPoint,
-      mapTypeId: google.maps.MapTypeId.TERRAIN
-    });
+function setupMarkerForSite(map, site) {
+  var coordinates = { lng: parseFloat(site.longitude), lat: parseFloat(site.latitude) };
+  var marker = new google.maps.Marker({
+    position: coordinates,
+    map: map,
+    title: site.name,
+  });
 
-    var setMarkers = function(response) {
-      if (typeof response !== "undefined") {
-        response.forEach(function(site) {
-          var coordinates = { lng: parseFloat(site.longitude), lat: parseFloat(site.latitude) };
-          var marker = new google.maps.Marker({
-            position: coordinates,
-            map: map,
-            title: site.name,
-          });
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">' + site.name + '</h1>'+
+      '<div id="bodyContent">'+
+      '<p>' + site.description + '</p>' +
+      '<p>Information: <a href="http://www.gaultschool.org/Home.aspx", target="_blank">' +
+      'http://www.gaultschool.org/Home.aspx' +
+      '</a></p>'+
+      // '<img src="http://www.texasbeyondhistory.net/gault/images/2001tamu-sm2.jpg", width="250px"' +
+      '</div>' + '</div>';
 
-          var contentString = '<div id="content">'+
-              '<div id="siteNotice">'+
-              '</div>'+
-              '<h1 id="firstHeading" class="firstHeading">' + site.name + '</h1>'+
-              '<div id="bodyContent">'+
-              '<p>' + site.description + '</p>' +
-              '<p>Information: <a href="http://www.gaultschool.org/Home.aspx", target="_blank">' +
-              'http://www.gaultschool.org/Home.aspx' +
-              '</a></p>'+
-              // '<img src="http://www.texasbeyondhistory.net/gault/images/2001tamu-sm2.jpg", width="250px"' +
-              '</div>' + '</div>';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
 
-          var infowindow = new google.maps.InfoWindow({
-            content: contentString
-          });
+  marker.addListener('mouseover', function () {
+    infowindow.open(map, marker);
+  });
 
-          marker.addListener('mouseover', function () {
-            infowindow.open(map, marker);
-          });
+  marker.addListener('mouseout', function () {
+    if(map.zoom === 4) {
+      infowindow.close();
+    }
+  });
 
-          marker.addListener('mouseout', function () {
-            if(map.zoom === 4) {
-              infowindow.close();
-            }
-          });
+  marker.addListener('click', function() {
+    map.setZoom(11);
+    map.setCenter(marker.getPosition());
+    infowindow.open(map, marker);
+  });
 
-          marker.addListener('click', function() {
-            map.setZoom(11);
-            map.setCenter(marker.getPosition());
-            infowindow.open(map, marker);
-          });
-
-          google.maps.event.addListener(infowindow,'closeclick',function(){
-            resetMap(map);
-          });
-        });
-      }
-    };
-    setMarkers(response);
-  }
-};
+  google.maps.event.addListener(infowindow,'closeclick',function(){
+    resetMap(map);
+  });
+}
